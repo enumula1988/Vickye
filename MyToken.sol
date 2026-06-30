aa0437e4f650c164ec992b843c3a3367cde50efa
enumula1988 <varu9926@gmail.com>
2026-06-30 11:58:07 +0530

    Add Solidity smart contract

---
+++ MyToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MyToken {
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint256 public totalSupply = 1000000;
+}
